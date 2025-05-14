f04942e0a895955ee59ce908d111666fe26b983b
Pranav Mishra <pranavmishra2101@gmail.com>
2025-05-14 21:49:18 +0545

    initial app

---
+++ metaverse/apps/http/src/routes/v1/admin.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+export const adminRouter = Router();
+
+adminRouter.get('/element', (req, res)=>{
+
+})
+
+adminRouter.put("/element/:elementId", (req,res)=>{
+    
+})
+adminRouter.get("/avatar", (req,res)=>{
+
+})
+adminRouter.put("/:map", (req,res)=>{
+
+})
