065e870f8e7ac20a3c8736c7d71d2c23b4780518
Pranav Mishra <pranavmishra2101@gmail.com>
2025-10-26 07:35:00 +0545

    testing change

--- metaverse/apps/frontend/src/service/spaceService.ts
+++ metaverse/apps/frontend/src/service/spaceService.ts
@@ -5,6 +5,7 @@ const API = axios.create({ baseURL: BACKEND_URL });
 const token = localStorage.getItem('authToken');
 
 export const spaceService = {
+
   myspace: async () => {
     const res = await API.get('/space/all', {
       headers: {
